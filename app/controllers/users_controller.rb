48d44c0209723ca8dc3840a28c05c577efdaf2a5
Doug Helferich <dhelf17@gmail.com>
2014-03-27 22:52:20 -0400

    Fixed an encoding issue, began work on user update method -dh

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,12 @@ class UsersController < ApplicationController
 	def edit
 		@user = User.find(params[:id])
 	end
+	
+	def update
+		user = User.find_by(id: params[:id])
+		user.name = params[:name]
+		if user.save
+			redirect_to @user
+		end
+	end	
 end
